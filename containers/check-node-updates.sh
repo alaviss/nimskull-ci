99586b9ba4a06b7e19717fd12ab591a56d4dde23
alaviss <leorize+oss@disroot.org>
2021-12-08 07:07:07 +0000

    containers: rename to Dockerfile and enable dependency tracking (#8)
    This pull renames Containerfile to Dockerfile, as tooling for Docker are more abundant, so we could not switch to OCI names yet.
    
    As part of this, we enable dependency tracking for our containers via dependabot and a custom script to track nodejs version.

--- containers/check-node-updates.sh
+++ containers/check-node-updates.sh
@@ -6,7 +6,7 @@ set -eu -o pipefail
 scriptDir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 
 # Grab the containers NodeJS version
-containerVer=$(grep -m 1 -F "node_version=" "$scriptDir/Containerfile" | cut -d'=' -f 2)
+containerVer=$(grep -m 1 -F "node_version=" "$scriptDir/Dockerfile" | cut -d'=' -f 2)
 
 echo "Containers version: $containerVer"
 
@@ -19,19 +19,24 @@ if [[ $latestLts == "$containerVer" ]]; then
   echo "Containers' NodeJS is up-to-date"
 else
   echo "Containers' NodeJS is outdated, performing update"
-  temporary=$(mktemp "$scriptDir/Containerfile.tmp.XXXXXXXXXX")
+  temporary=$(mktemp "$scriptDir/Dockerfile.tmp.XXXXXXXXXX")
   # Write the update to a temporary
   #
   # This sed script captures everything up to `node_version=`, then change the
   # portion after the `=` to the latest version
-  sed 's/\(.*node_version=\).*/\1'"$latestLts"'/' "$scriptDir/Containerfile" > "$temporary"
+  sed 's/\(.*node_version=\).*/\1'"$latestLts"'/' "$scriptDir/Dockerfile" > "$temporary"
   # Show the diff
-  diff -u "$scriptDir/Containerfile" "$temporary" || {
+  diff -u "$scriptDir/Dockerfile" "$temporary" || {
     # Diff returns either 1 or 0 on success, so exit failure otherwise
     if [[ $? -ne 1 && $? -ne 0 ]]; then
       exit "$?"
     fi
   }
   # Then replace the original file
-  mv -v "$temporary" "$scriptDir/Containerfile"
+  mv -v "$temporary" "$scriptDir/Dockerfile"
+
+  # Integration with CI by emitting the target version
+  if [[ -v GITHUB_ACTIONS ]]; then
+    echo "::set-output name=updated-to::$latestLts"
+  fi
 fi
